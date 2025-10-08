09b183cb57893c15eb12666272743d4e7dc17d4d
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-10-08 18:22:35 +0200

    Cleanup model

--- Models/Settings.cs
+++ Models/Settings.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace FikaHeadlessManager.Models;
 
-namespace FikaHeadlessManager.Models
+public record Settings
 {
-    public record Settings
-    {
-        public string? ProfileId { get; set; }
-        public Uri? BackendUrl { get; set; }
-    }
+    public string? ProfileId { get; set; }
+    public Uri? BackendUrl { get; set; }
 }
